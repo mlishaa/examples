c4060bc09c8d0a4b7d26f675b0b1d35906facfdd
mlishaa <57273606+mlishaa@users.noreply.github.com>
2019-11-05 14:10:38 -0500

    first commit

---
+++ examples/examples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while(num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+
+            }
+            Console.WriteLine(" sum is {0} ", sum);
+            Console.ReadLine();
+        }
+    }
+}
